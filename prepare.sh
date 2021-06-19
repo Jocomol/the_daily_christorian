7f7c14fe785994b8280421d1ee67389c569ec59a
Jocomol <joelmeier08@gmail.com>
2021-06-19 14:47:58 +0200

    finished rss feed

---
+++ prepare.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+rm -rf daily_chris_chan/*
+cp empty_rss.xml rss.xml
--- the_daily_christorian.py
+++ the_daily_christorian.py
@@ -2,6 +2,8 @@
 from bs4 import BeautifulSoup
 from jinja2 import Environment, FileSystemLoader
 
+counter_text = ["days since Chris made up a new escapist fantasy to cope with reality", "days since the Dimensional Merge was to be completed","days since Chris joined the cult of the Idea Guys", "days since Chris last begged for money","days since Chris last up    loaded pages to the Sonichu comic", "days since Chris last applied for a job"]
+
 data = {
     "week": {
         "link": None,
@@ -9,8 +11,7 @@ data = {
         "picture": None,
         "text": None,
     },
-    "coping_counters": [],
-    "this_day": {
+    "day": {
         "text": None,
         "link": None,
         "picture": None
@@ -18,23 +19,11 @@ data = {
     },
     "quote": None,
     "quoted": None,
-    "begging_counters": [],
-    "ongoing": [] 
+    "counter":[], 
+    "ongoing": [],
+    "dates": []
 
 }
-article_of_the_week_link = None
-article_of_the_week_title = None
-article_of_the_week_picture = None
-article_of_the_week_text = "" 
-coping_counters = []
-this_day = None 
-this_day_text = None
-this_day_link = None 
-this_day_picture = None
-quote = None 
-quoted = None 
-ongoing = [] 
-dates = []
 
 with open("/tmp/cwcki_source/cwcki") as source:
     soup = BeautifulSoup(source, features="html5lib")
@@ -43,29 +32,31 @@ with open("/tmp/cwcki_source/cwcki") as source:
     data["week"]["title"] = article_of_the_week["title"]
     data["week"]["link"] = "https://sonichu.com" + article_of_the_week["href"]
     data["week"]["picture"] = "https://sonichu.com" + soup.body.find("img", class_="thumbimage")["src"]
-    article_of_the_week_text_para = soup.body.find("div", class_="thumb tright").find_next_sibling("p") 
+    article_of_the_week_text_para = soup.body.find("div", class_="thumb tright").find_next_sibling("p")
+    article_of_the_week_text = ""
     while article_of_the_week_text_para is not None:
         if "..." not in article_of_the_week_text_para.get_text():
             article_of_the_week_text += article_of_the_week_text_para.get_text()
         article_of_the_week_text_para = article_of_the_week_text_para.find_next_sibling("p")
     data["week"]["text"] = article_of_the_week_text
+    i=0
     for counter in soup.body.find_all("div", style="font-size:2em"):
-        coping_counters.append(counter.get_text().replace(",",""))
-    
+        data["counter"].append(counter.get_text().replace(",","") + " " + counter_text[i])
+        i+=1
     this_day = soup.body.find("div", style="float:left;margin-right:0.9em")
     para = this_day.find_all("p")
-    this_day_text = para[0].get_text()
-    this_day_link = "https://sonichu.com" + para[1].find("b").find("a")["href"]
-    this_day_picture = "https://sonichu.com" + this_day.find("a", class_="image")["href"]
+    data["day"]["text"] = para[0].get_text()
+    data["day"]["link"] = "https://sonichu.com" + para[1].find("b").find("a")["href"]
+    data["day"]["picture"] = "https://sonichu.com" + this_day.find("a", class_="image")["href"]
     
     topic = soup.body.find("b", string="Ongoing").find_next_sibling("a")
     while topic is not None:
-        ongoing.append(topic.get_text())
+        data["ongoing"].append(topic.get_text())
         topic = topic.find_next_sibling("a")
     
     dates_array = soup.body.find("b", string="Ongoing").parent.find_next_sibling("ul").find_all("li")
     for date in  dates_array:
-        dates.append(date.get_text())
+        data["dates"].append(date.get_text())
     
     text = soup.body.find("h2", string="Quote of the Now").parent.parent.find_next_sibling("tr").get_text()
     
@@ -73,10 +64,10 @@ with open("/tmp/cwcki_source/cwcki") as source:
     for line in text.splitlines():
         if line.strip() and len(line) > 1:
             if first:
-                quote = line
+                data["quote"] = line
                 first = False 
             else:
-                quoted = line 
+                data["quoted"] = line 
 
 file_loader = FileSystemLoader('templates')
 env = Environment(loader=file_loader)
@@ -84,34 +75,3 @@ template = env.get_template("template.html")
 output = template.render(data=data)
 
 print(output)
-
-#print("---")
-#print("Article Of the Week")
-#print(article_of_the_week_link) 
-#print(article_of_the_week_title)
-#print(article_of_the_week_picture)
-#print(article_of_the_week_text)
-#print("---")
-#
-#print("This day")
-#print(this_day_link)
-#print(this_day_text)
-#print(this_day_picture)
-#print("---")
-#
-#print("Ongoing")
-#for item in ongoing:
-#    print(item)
-#for date in dates:
-#    print(date)
-#print("---")
-#
-#print("Counter")
-#for coping_counter in coping_counters:
-#   print(coping_counter)
-#print("---")
-#
-#print("Quotes")
-#print(quote)
-#print(quoted)
-#print("---")
